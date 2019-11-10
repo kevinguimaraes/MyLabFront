016bb5af7ee17a01edb34cb57922a3bd71f61c22
Kevin <kevinsiob@hotmail.com>
2019-11-10 18:31:32 -0300

    add amostra

---
+++ src/app/amostra/create-amostra/create-amostra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-amostra',
+  templateUrl: './create-amostra.component.html',
+  styleUrls: ['./create-amostra.component.css']
+})
+export class CreateAmostraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { CreateAmostraComponent } from './amostra/create-amostra/create-amostra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    CreateAmostraComponent
   ],
   imports: [
     BrowserModule,
